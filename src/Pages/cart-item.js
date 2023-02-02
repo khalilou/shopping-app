ac45fff3831357f9cd2a414768f30ba15ee011f9
khalilou <khalil.al@hotmail.com>
2023-02-03 00:49:49 +0100

    set cartitem

---
+++ src/Pages/cart-item.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const CartItem = (props) => {
+    const { id, productName, price, productImage } = props.data;
+  return (
+    <div className="CartItem">
+        <img src={productImage} alt="" />
+        <div className="description">
+            <p>
+                {" "}
+                <b>{productName}</b>
+            </p>
+            <p>${price}</p>
+        </div>
+    </div>
+  )
+}
--- src/Pages/cart.js
+++ src/Pages/cart.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {ShopContext} from '../shop-context'; 
+import { PRODUCTS } from '../products';
+import { useContext } from 'react';
+import { CartItem } from './cart-item';
 
 export const Cart = () => {
+  const { cartItems } = useContext(ShopContext);
   return (
-    <div>Cart</div>
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if(cartItems[product.id] !== 0) {
+            return <CartItem data={product} />
+          }
+        })}
+      </div>
+    </div>
   )
 }
